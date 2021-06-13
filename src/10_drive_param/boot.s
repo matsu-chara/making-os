68ceb49bc69b0b18fb6d6ddc7e045ed7af779e1f
matsu-chara <matsuy00@gmail.com>
2021-06-13 20:34:19 +0900

    print drive param

--- src/10_drive_param/boot.s
+++ src/10_drive_param/boot.s
@@ -50,7 +50,7 @@ BOOT:
         at  drive.sect,     dw  2
     iend
 
-; module
+; module 
 %include "../modules/real/puts.s"
 %include "../modules/real/reboot.s"
 %include "../modules/real/read_chs.s"
@@ -59,12 +59,41 @@ BOOT:
     times 510 - ($ - $$) db 0x00    ; 先頭512バイトの終了
     db 0x55, 0xAA
 
+; module 先頭512byte以降に設置
+%include "../modules/real/itoa.s"
+%include "../modules/real/get_drive_param.s"
+
 stage_2:
-    cdecl puts, .s0
-    jmp $                               ; while(1); (無限ループ)
+        cdecl puts, .s0
+
+        ; get drive param
+        cdecl   get_drive_param, BOOT         ; get_drive_param(DX, BOOT.CYLN);
+        cmp     ax, 0
+.10Q:   jne     .10E
+.10T:   cdecl   puts, .e0
+        call    reboot
+.10E:
+        mov     ax, [BOOT + drive.no]
+        cdecl   itoa, ax, .p1, 2, 16, 0b0100
+        mov     ax, [BOOT + drive.cyln]
+        cdecl   itoa, ax, .p2, 4, 16, 0b0100
+        mov     ax, [BOOT + drive.head]
+        cdecl   itoa, ax, .p3, 2, 16, 0b0100
+        mov     ax, [BOOT + drive.sect]
+        cdecl   itoa, ax, .p4, 2, 16, 0b0100
+        cdecl   puts, .s1
+
+        jmp $                               ; while(1); (無限ループ)
 
 ; data
 .s0 db "2nd stage...", 0x0A, 0x0D, 0
+.s1 db " Drive:0x"
+.p1 db "  , C:0x"
+.p2 db "    , H:0x"
+.p3 db "  , S:0x"
+.p4 db "  ", 0x0A, 0x0D, 0
+
+.e0 db "Can't get drive parameter.", 0
 
 ; パディング（今後作成するコード量を見越して8KBのファイルにしている）
     times BOOT_SIZE - ($-$$) db 0
