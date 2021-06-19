5ecfe2dea664feb040b54225221a6dffe22b20a6
matsu-chara <matsuy00@gmail.com>
2021-06-19 10:58:32 +0900

    KBC cmd write

--- src/modules/real/kbc.s
+++ src/modules/real/kbc.s
@@ -31,10 +31,10 @@ KBC_Data_Read:
     push    cx
 
     mov     cx, 0
-.20L:
+.10L:
     in      al, 0x64        ; AL = inp(0x64) // KBCステータス
     test    al, 0x01        ; ZF = AL & 0x01 // 読み込み可能？
-    loopnz  .20L
+    loopnz  .10L
 
     cmp     cx, 0
     jz      .20E
@@ -44,6 +44,31 @@ KBC_Data_Read:
 
     mov     di, [bp + 4]     ; DI = ptr
     mov     [di + 0], ax    ; DI[0] = AX
+.20E:
+    mov     ax, cx
+
+    pop     cx
+    mov     sp, bp
+    pop     bp
+
+    ret
+KBC_Cmd_Write:
+    push    bp
+    mov     bp, sp
+
+    push    cx
+
+    mov     cx, 0
+.10L:
+    in      al, 0x64        ; AL = inp(0x64) // KBCステータス
+    test    al, 0x02        ; ZF = AL & 0x02 // 書き込み可能？
+    loopnz  .10L
+
+    cmp     cx, 0
+    jz      .20E
+
+    mov     al, [bp + 4]    ; AL = コマンド
+    out     0x64, al        ; outp(0x64, AL)
 .20E:
     mov     ax, cx
 
