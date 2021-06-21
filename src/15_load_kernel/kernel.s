eb6d5c71a805e95c65650e92eecb1cf94390cc2f
matsu-chara <matsuy00@gmail.com>
2021-06-22 08:02:34 +0900

    何もしないカーネル

---
+++ src/15_load_kernel/kernel.s
@@ -0,0 +1,10 @@
+%include "../include/define.s"
+%include "../include/macro.s"
+
+    ORG KERNEL_LOAD
+
+[BITS 32]
+kernel:
+    jmp $
+
+    times KERNEL_SIZE - ($ - $$) db 0
--- src/include/define.s
+++ src/include/define.s
@@ -4,3 +4,6 @@ SECT_SIZE   equ (512)   ; セクタサイズ
 BOOT_SECT   equ (BOOT_SIZE / SECT_SIZE) ; ブートプログラムのセクタ数
 
 E820_RECORD_SIZE    equ 20   ; BIOSで取得したメモリ情報を格納する領域のサイズ
+
+KERNEL_LOAD equ 0x0010_1000 ; カーネルのロードアドレス
+KERNEL_SIZE equ (1024 * 8); カーネルサイズ
