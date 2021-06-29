edac146158c2c8515f3dd343bc6b8cc1a91dbfa5
matsu-chara <matsuy00@gmail.com>
2021-06-30 08:58:16 +0900

    vga.s

---
+++ src/modules/protect/vga.s
@@ -0,0 +1,20 @@
+vga_set_read_plane:
+    push    ebp
+    mov     ebp, esp
+
+    push    eax
+    push    edx
+
+    mov     ah, [ebp + 8]       ; AH = プレーンを選択
+    and     ah, 0x03            ; 余計なビットをマスク
+    mov     al, 0x04            ; AL = 読み込みマップ選択レジスタ
+    mov     dx, 0x03CE          ; DX = グラフィクス制御ポート
+    out     dx, ax              ; ポート出力
+
+    pop     edx
+    pop     eax
+
+    mov     esp, ebp
+    pop     ebp
+
+    ret
