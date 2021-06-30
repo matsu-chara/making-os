6bde0435467e80677b7a81f5954b2bbd02ec5f91
matsu-chara <matsuy00@gmail.com>
2021-07-01 08:34:54 +0900

    vga set_write_plane

--- src/modules/protect/vga.s
+++ src/modules/protect/vga.s
@@ -16,5 +16,25 @@ vga_set_read_plane:
 
     mov     esp, ebp
     pop     ebp
+    ret
+
+vga_set_write_plane:
+    push    ebp
+    mov     ebp, esp
+
+    push    eax
+    push    edx
+
+    mov     ah, [ebp + 8]       ; AH = プレーンを選択
+    and     ah, 0x0F            ; 余計なビットをマスク
+    mov     al, 0x02            ; AL = 書き込みプレーンを指定
+    mov     dx, 0x03CE          ; DX = グラフィクス制御ポート
+    out     dx, ax              ; ポート出力
+
+    pop     edx
+    pop     eax
+
+    mov     esp, ebp
+    pop     ebp
 
     ret
