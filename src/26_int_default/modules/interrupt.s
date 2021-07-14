1b7265dcfc73caf2ea1f7284ab4a765a47107486
matsu-chara <matsuy00@gmail.com>
2021-07-15 08:08:06 +0900

    default_interrupt

---
+++ src/26_int_default/modules/interrupt.s
@@ -0,0 +1,32 @@
+int_stop:
+    ; EAXで示される文字列を表示
+    cdecl   draw_str, 25, 15, 0x060F, eax
+
+    ; stackのデータを文字列に変換
+    mov     eax, [esp + 0]
+    cdecl   itoa, eax, .p1, 8, 16, 0b0100
+
+    mov     eax, [esp + 4]
+    cdecl   itoa, eax, .p2, 8, 16, 0b0100
+
+    mov     eax, [esp + 8]
+    cdecl   itoa, eax, .p3, 8, 16, 0b0100
+
+    mov     eax, [esp + 12]
+    cdecl   itoa, eax, .p4, 8, 16, 0b0100
+
+    cdecl   draw_str, 25, 16, 0x0F04, .s1
+    cdecl   draw_str, 25, 17, 0x0F04, .s2
+    cdecl   draw_str, 25, 18, 0x0F04, .s3
+    cdecl   draw_str, 25, 19, 0x0F04, .s4
+
+    jmp     $
+
+.s1 db "ESP+ 0:"
+.p1 db "________ ", 0
+.s2 db "   + 4:"
+.p2 db "________ ", 0
+.s3 db "   + 8:"
+.p3 db "________ ", 0
+.s4 db "   +12:"
+.p5 db "________ ", 0
