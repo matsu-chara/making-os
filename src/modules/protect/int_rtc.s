e2b6d3632c827944622260f49d853273acb35182
matsu-chara <matsuy00@gmail.com>
2021-07-22 10:05:28 +0900

    int_rtc

---
+++ src/modules/protect/int_rtc.s
@@ -0,0 +1,26 @@
+int_rtc:
+    pusha
+    push    ds
+    push    es
+
+    ; データ用セグメントセレクタの設定
+    mov     ax, 0x0010                  ; GDTの先頭からのバイト数 (8バイトで構成される要素の2番目)
+    mov     ds, ax
+    mov     es, ax
+
+    cdecl   rtc_get_time, RTC_TIME
+
+    ; RTCの割り込み要因を取得
+    outp    0x70, 0x0c
+    in      al, 0x71                    ; RTCの内部レジスタCをALに読み込むと割り込み要因がクリアされる。（読み取った値は使わない）
+
+    ; 割り込みコントローラにEOI(End Of Interrupt）コマンドを送信
+    mov     al, 0x20
+    out     0xA0, al                    ; スレーブPIC
+    out     0x20, al                    ; マスターPIC
+
+    pop     es
+    pop     ds
+    popa
+    
+    iret                                ; フラグレジスタも含めて復帰する命令
