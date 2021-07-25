94ec4ab69d678cdebfd84a855ef712927c7c8b40
matsu-chara <matsuy00@gmail.com>
2021-07-25 17:54:02 +0900

    fix bug

--- src/modules/protect/ring_buff.s
+++ src/modules/protect/ring_buff.s
@@ -2,7 +2,6 @@ ring_rd:
     push    ebp
     mov     ebp, esp
 
-    push    eax
     push    ebx
     push    esi
     push    edi
@@ -17,7 +16,7 @@ ring_rd:
     cmp     ebx, [esi + ring_buff.wp]
     je      .10E
 
-    mov     al, [esi + ring_buiff.item + ebx]
+    mov     al, [esi + ring_buff.item + ebx]
     mov     [edi], al
 
     inc     ebx
@@ -30,7 +29,6 @@ ring_rd:
     pop     edi
     pop     esi
     pop     ebx
-    pop     eax
 
     mov     esp, ebp
     pop     ebp
@@ -41,14 +39,12 @@ ring_wr:
     push    ebp
     mov     ebp, esp
 
-    push    eax
     push    ebx
+    push    ecx
     push    esi
-    push    edi
 
     ; 引数
     mov     esi, [ebp + 8]      ; ESI = リングバッファ 
-    mov     edi, [ebp + 12]     ; EDI = データアドレス
 
     ; 書き込み位置を確認
     mov     eax, 0
@@ -67,12 +63,44 @@ ring_wr:
     mov     eax, 1
 
 .10E:
-    pop     edi
-    pop     esi
-    pop     ebx
-    pop     eax
+    pop		esi
+    pop		ecx
+    pop		ebx
 
     mov     esp, ebp
     pop     ebp
 
     ret
+
+draw_key:
+    push    ebp
+    mov     ebp, esp
+
+    pusha
+
+    mov		edx, [ebp + 8]					; EDX = X（列）;
+    mov		edi, [ebp +12]					; EDI = Y（行）;
+    mov		esi, [ebp +16]					; ESI = リングバッファ;
+
+    mov     ebx, [esi + ring_buff.rp]
+    lea     esi, [esi + ring_buff.item]
+    mov     ecx, RING_ITEM_SIZE
+.10L:
+    dec     ebx
+    and     ebx, RING_INDEX_MASK
+    mov     al, [esi + ebx]
+
+    cdecl   itoa, eax, .tmp, 2, 16, 0b0100
+    cdecl   draw_str, edx, edi, 0x02, .tmp
+    add     edx, 3
+    loop    .10L
+
+.10E:
+
+    popa
+
+    mov     esp, ebp
+    pop     ebp
+    ret
+
+.tmp    db "-- ", 0
