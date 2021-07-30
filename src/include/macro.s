d074121918ff0828e6521c062362640c9c9bb62a
matsu-chara <matsuy00@gmail.com>
2021-07-30 20:01:31 +0900

    macro

--- src/include/macro.s
+++ src/include/macro.s
@@ -49,3 +49,21 @@ struc ring_buff
     .wp     resd    1
     .item   resb    RING_ITEM_SIZE
 endstruc
+
+%macro  set_desc 2-*
+    push    eax
+    push    edi
+
+    mov     edi, %1
+    mov     eax, %2
+
+%if 3 == %0
+    mov     [edi + 0], %3
+%endif
+    mov     [edi + 2], ax
+    shr     eax, 16
+    mov     [edi + 4], al
+    mov     [edi + 7], ah
+
+    pop     edi
+    pop     eax
