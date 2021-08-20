7dcc67007952830b00448f95924a7af07a18a8bd
matsu-chara <matsuy00@gmail.com>
2021-08-20 22:18:12 +0900

    ctrl_alt_end

--- src/include/define.s
+++ src/include/define.s
@@ -33,3 +33,24 @@ PARAM_TASK_6 equ 0x0010_A000    ; 描画パラメータ
 CR3_TASK_4   equ 0x0020_0000    ; ページ変換テーブル
 CR3_TASK_5   equ 0x0020_2000    ; ページ変換テーブル
 CR3_TASK_6   equ 0x0020_4000    ; ページ変換テーブル
+
+FAT_SIZE     equ        (1024 * 128)    ; FAT-1/2
+ROOT_SIZE    equ        (1024 *  16)    ; ルートディレクトリ領域
+
+ENTRY_SIZE   equ        32                ; エントリサイズ
+
+; FAT関係
+FAT_OFFSET   equ        (BOOT_SIZE + KERNEL_SIZE)
+FAT1_START   equ        (KERNEL_SIZE)
+FAT2_START   equ        (FAT1_START + FAT_SIZE)
+ROOT_START   equ        (FAT2_START + FAT_SIZE)
+FILE_START   equ        (ROOT_START + ROOT_SIZE)
+
+; ファイル属性
+ATTR_READ_ONLY equ        0x01
+ATTR_HIDDEN    equ        0x02
+ATTR_SYSTEM    equ        0x04
+ATTR_VOLUME_ID equ        0x08
+ATTR_DIRECTORY equ        0x10
+ATTR_ARCHIVE   equ        0x20
+
